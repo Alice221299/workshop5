bcd6d3cf5cd0d43ccaf126e05d1eeefc77757b0a
snaidy giraldo <snaidy98g@gmail.com>
2023-08-14 15:02:44 -0600

    :art:create navar

--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -45,11 +45,11 @@ const Router = () => {
                 </Route>
 
                 <Route element={<PrivateRouter isAutenticated={userLogin.isAutenticated}/>}>
-                    {/* <Route element={<Layout/>}> */}
+                    <Route element={<Layout/>}>
                         <Route index element={<Home/>}/>
                         <Route path='profile' element={<Profile/>}/>
                         <Route path='publication' element={<Publication/>}/>
-                    {/* </Route> */}
+                    </Route>
                 </Route>
             </Route>
         </Routes>

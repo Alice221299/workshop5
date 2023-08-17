61a0a7372eea804166a643ce1b6c2f04f83a6f3a
Alice <ann.bondarets22@gmail.com>
2023-08-17 09:28:21 -0500

    Added tags section

--- src/components/profileTags/ProfileTags.jsx
+++ src/components/profileTags/ProfileTags.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import getPosts from '../../services/postsService'
+import { getSession } from '../../services/sessionService'
 
 const ProfileTags = () => {
+    const [posts, setPosts] = useState()
+    const user = getSession()
+
+    useEffect(() => {
+        getAllPosts()
+        if (posts) {
+            const taggedPosts = posts.filter(post => post.tags.find(tag => tag === user.id))
+            setPosts(taggedPosts)
+        }
+    }, [])
+
+    
+
+    const getAllPosts = async () => {
+        const response = await getPosts()
+        setPosts(response)
+        console.log(response);
+    }
   return (
-    <div>ProfileTags</div>
+    <div>
+        {posts &&
+            posts.map((post) => (
+                <figure className='pictures-element' key={post.id}>
+                    <img  src={post.image} alt="Image" />
+                </figure>
+            ))
+        }
+    </div>
   )
 }
 

c6443b847f392d969a5c36b0c1c35a9ad57da8ba
Stefannya <stefannyaonofre@gmail.com>
2023-08-18 10:38:18 -0500

    organizacion nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,50 +1,30 @@
-import React, { useContext, useState } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom';
+import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom';
 import './nav.scss';
 import home from '/house.svg';
 import search from '/search.svg';
 import more from '/more.svg';
 import bell from '/bell.svg';
-import user from '/user.svg';
 import { AppContext } from '../../routers/Router';
-import NewPublication from '../newPublication/NewPublication';
 const Nav = () => {
-  const location = useLocation();
-  const path = location.pathname;
   const navigate = useNavigate();
   const {user: {userLogin}} = useContext(AppContext);
-  const [opened, setOpened] = useState(false);
 
   const handleProfile = () => {
     navigate('/profile')
   }
 
   const handleNewPublication = () => {
-    setOpened(true)
     navigate('/newPublication')
-  }
-
-  const handleCloseForm = () => {
-    setOpened(false);
   };
  
   return (
     <>
-    {/* {(path === "/" || path === "/publication") ? (
-     <div className='nav'>
-      NAVAR
-     </div>
-     ) : (
-      ""
-    )} */}
     <div className='nav'>
       <figure className='nav__menu'>
         <img className='icon' src={home} alt="" />
         <img className='icon' src={search} alt="" />
         <img className='more' src={more} alt="" onClick={handleNewPublication} />
-        {/* {
-          opened ? (<NewPublication onClose={handleCloseForm}/>) : ""
-        } */}
         <img className='icon' src={bell} alt="" />
         <img className='icon image' src={userLogin?.user.avatar} alt="" onClick={handleProfile}/>
       </figure>
